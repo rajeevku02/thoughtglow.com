828e1760d4d652aec42757d5d79f66e0df5de076
Rajeev Kumar <rajeevku02@gmail.com>
2024-02-06 16:06:54 +0530

    Update

--- assets/js/nav_util.js
+++ assets/js/nav_util.js
@@ -1,18 +1,5 @@
-const apiBaseUrl = 'https://api.lekh.app';
-
-function handleRef() {
-  var ref = new URL(window.location).searchParams.get("ref");
-    if (ref) {
-      var xhttp = new XMLHttpRequest();
-      var url = apiBaseUrl + "/hello?ref=" + ref;
-      xhttp.open("GET", url);
-      xhttp.withCredentials = true;
-      xhttp.send();
-    }
-}
 
 document.addEventListener("DOMContentLoaded", () => {
-  handleRef();
 
   const openNav = document.querySelector("#open-nav");
   const closeNav = document.querySelector("#close-nav");
